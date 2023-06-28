382d9a8323867caed3289937b9eed41b77cc5730
KatharineBond <133820352+KatharineBond@users.noreply.github.com>
2023-06-28 15:49:11 +0300

    Update MySQL connection.py

--- MySQL connection.py
+++ MySQL connection.py
@@ -1,2 +1,16 @@
-from getpass import getpass
-from mysql.connector import connect, Error
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password):
+	connection = None
+	try:
+		connection = mysql.connector.connect(
+			host=host_name,
+            user=user_name,
+			passwd=user_password
+		)
+		print("Подключение к базе данных MySQL прошло успешно")
+	except Error as e:
+		print(f"Произошла ошибка '{e}'")
+	return connection
+connection = create_connection("127.0.0.1", "root", "1234")
