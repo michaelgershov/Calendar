15bf71ad076e5bdbdc8351d46fbfee5cd62a5cdd
Владислав Коряко <vlkoryako@gmail.com>
2023-06-30 11:32:55 +0300

    Забыл этот файл обновить

--- data_calendar.py
+++ data_calendar.py
@@ -1,15 +1,21 @@
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 import datetime
 import calendar
+
+
 def create_callback_data(action, year, month, day):
     return ";".join([action, str(year), str(month), str(day)])
+
 
 def separate_callback_data(data):
     return data.split(";")
 
-def create_calendar(year=None, month=None):
 
-    now = datetime.datetime.now()
+def create_calendar(year=None, month=None, time=1):
+    if time == 1:
+        now = datetime.datetime.now()
+    else:
+        now = time
     if year is None:
         year = now.year
     if month is None:
@@ -23,20 +29,24 @@ def create_calendar(year=None, month=None):
     for day in ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]:
         row.append(InlineKeyboardButton(day, callback_data=data_ignore))
     keyboard.append(row)
-
     my_calendar = calendar.monthcalendar(year, month)
     for week in my_calendar:
         row=[]
         for day in week:
-            if day == 0:
+            if ((day == 0) or ((day > 0 and day < now.day) and (month == now.month) and (year == now.year))):
                 row.append(InlineKeyboardButton(" ", callback_data=data_ignore))
             else:
                 row.append(InlineKeyboardButton(str(day), callback_data=create_callback_data("День", year, month, day)))
         keyboard.append(row)
     row=[]
-    row.append(InlineKeyboardButton("<", callback_data=create_callback_data("Пред-месяц", year, month, day)))
-    row.append(InlineKeyboardButton(" ", callback_data=data_ignore))
-    row.append(InlineKeyboardButton(">", callback_data=create_callback_data("След-месяц", year, month, day)))
-    keyboard.append(row)
+    if month == now.month:
+        row.append(InlineKeyboardButton(" ", callback_data=data_ignore))
+        row.append(InlineKeyboardButton(">", callback_data=create_callback_data("След-месяц", year, month, day)))
+        keyboard.append(row)
+    else:
+        row.append(InlineKeyboardButton("<", callback_data=create_callback_data("Пред-месяц", year, month, day)))
+        row.append(InlineKeyboardButton(" ", callback_data=data_ignore))
+        row.append(InlineKeyboardButton(">", callback_data=create_callback_data("След-месяц", year, month, day)))
+        keyboard.append(row)
 
     return InlineKeyboardMarkup(keyboard)
